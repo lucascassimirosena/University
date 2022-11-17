b99fd1aadb6771ab4a0b2fa76f22a5a51a0eb352
Lucas Cassimiro <lucas.cassimiro@live.com>
2022-11-17 08:28:41 -0300

    studying c

---
+++ gamesEstacio/2ndSemester/P_Estruturada/1.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main()
+{
+    printf( sizeof(int) );
+}
+
+/* The sizeof function returns the number of byte a variable has, it can also show the number of bytes a data type has. This function was something I learnt at the College.*/
